3099536bf887eabfbf124667936fc607c479c44d
Kazuhiro Kubota <kazu@k2lab.net>
2014-07-15 20:57:48 +0900

    implement debug task

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -6,7 +6,7 @@ var dart = require('gulp-dart2js');
 gulp.task('default', ['build']);
 
 gulp.task('debug', function(){
-  
+  gulp.watch('app/dart/*.dart', ['build']);
 });
 
 gulp.task('build', function(){
